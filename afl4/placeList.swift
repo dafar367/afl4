a46c7d3d0beba17a4c99d209c9c01e84bf41c342
dafar367 <dnafi2822@gmail.com>
2022-06-07 08:17:31 +0700

    5

--- afl4/placeList.swift
+++ afl4/placeList.swift
@@ -9,9 +9,32 @@ import SwiftUI
 
 struct placeList: View {
     var destination: [Destination]
+    @State var searchText = ""
+    @State var sear = false
     var body: some View {
         VStack{
             HStack{
+                TextField("Search", text: $searchText)
+                                   .padding(.leading,20)
+                           }
+                           .padding()
+                           .padding(.horizontal)
+                           .background(Color(.systemGray4))
+                           .cornerRadius(10)
+                           .onTapGesture {
+                               sear = true
+                           }
+                           .overlay(
+                            HStack{
+                                Image(systemName: "magnifyingglass")
+                                    .foregroundColor(.gray)
+                                Spacer()
+                                if sear{
+                                    Button(action: {searchText = ""}, label: {Image(systemName: "xmark.circle.fill")
+                                        .foregroundColor(.gray)})
+                                }
+                            }.padding(.horizontal,20))
+            
                 Text("\(destination.count) \("destination")")
                     .font(.headline)
                     .fontWeight(.medium)
@@ -22,13 +45,12 @@ struct placeList: View {
             LazyVGrid(columns:
                         [GridItem(.adaptive(minimum: 140),spacing: 30)],
                       spacing: 30) {
-                ForEach(destination){
+                ForEach((destination).filter({"\($0)".contains(searchText)||searchText.isEmpty})) {
                     destination in
                     NavigationLink(destination: placeDetail(destination: destination)){
                         placeCard(destination: destination)
                     }
                 }
-            }
             .padding(.top)
         }
         .padding(.horizontal)
